dd6f1e71442be1e2c86491c33b7b3a08ac3241a1
Owen864720655 <864720655@qq.com>
2017-01-19 20:40:47 +0800

    update

---
+++ codes/src/learn_java/Thundering.java
@@ -0,0 +1,18 @@
+package learn_java;
+
+public class Thundering {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "lili";
+		
+		System.out.println(str + "µÄÄêÁäÊÇ:");
+		
+//		int age = Integer.parseInt("20"); //OK
+		int age = Integer.parseInt("20L");
+
+		
+		System.out.println(age);
+	}
+
+}
--- codes/src/learn_java/tran.java
+++ codes/src/learn_java/tran.java
@@ -9,13 +9,17 @@ public class tran {
 		if (number1 < 0 || number2 < 0) {
 			throw new MyException("不可以使用负数");
 		}
+		
+		if (number1 > 100 || number2 > 100) {
+			throw new MyException("数值太大了");
+		}
 		return (number1+number2)/2;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			int result = avg(-10, 150);
+			int result = avg(10, 150);
 			System.out.println(result);
 		} catch (MyException e) {
 			System.out.println(e);
